858ac8e87d5bd6e13f9ee7cebd072661d8b6c4f0
NachoR9 <nachorm9@hotmail.com>
2024-09-18 20:16:32 -0700

    header created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <header>
-
-    <div>
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <Header />
 
   <RouterView />
 </template>
