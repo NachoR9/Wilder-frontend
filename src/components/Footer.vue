9f5f7b593915399b3619e1edd70c2250b021706a
NachoR9 <nachorm9@hotmail.com>
2024-09-22 17:05:29 -0700

    footer done with redirection to my github profile

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="bg-gradient-to-r from-indigo-500 via-purple-500 to-red-500 flex gap-6 items-center justify-between px-4 py-4">
+        <span class="text-black">Copyright 2024 &#174;</span>
+        <span class="grow ml-9 text-2xl text-black font-mono absolute left-1/2 ">Wilder</span> 
+        <a href="https://github.com/NachoR9" target="_blank">
+        <img src="../assets/images/avatar.jpeg" alt="github profile" class="inline size-8 rounded-full mr-10"/>
+        </a>
+    </footer>
+</template>
