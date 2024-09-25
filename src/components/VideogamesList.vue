cf9ac9a4a6ece3782cf8f59e3789e96126603328
NachoR9 <nachorm9@hotmail.com>
2024-09-24 20:51:22 -0700

    print videogames list

--- src/components/VideogamesList.vue
+++ src/components/VideogamesList.vue
@@ -12,6 +12,26 @@ onMounted(() => {
 </script>
 
 <template>
-    <div>{{ videogameStore.videogames }}</div>
-
+    <div class="px-8 py-8">
+    <ul role="list" class="divide-y divide-gray-200">
+            <li v-for="videogame in videogameStore.videogames" class="py-8">
+                <div class="flex items-center">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-8 rounded-full" src="" :alt="videogame.name">
+                    </div>
+                    <div class="flex-1 min-w-0 ms-12">
+                        <p class="text-lg font-medium text-gray-900 truncate">
+                            {{videogame.name}}
+                        </p>
+                        <p class="text-md text-gray-500 truncate">
+                            {{videogame.genres.join(", ")}}
+                        </p>
+                    </div>
+                    <div class="inline-flex items-center text-md font-semibold text-gray-900">
+                        {{videogame.platform}}
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
