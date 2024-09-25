5f4f399eac02c16cf52148057baa5d15bf652a5c
NachoR9 <nachorm9@hotmail.com>
2024-09-24 20:23:40 -0700

    fetch videogames list from api

--- src/components/VideogamesList.vue
+++ src/components/VideogamesList.vue
@@ -1,9 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
-import {  } from '@/stores'; 
+import { useVideogameStore } from '@/stores/videogames';
+
+const videogameStore = useVideogameStore();
+
+onMounted(() => {
+  videogameStore.fetchVideogames();
+});
+
 </script>
 
 <template>
+    <div>{{ videogameStore.videogames }}</div>
 
 </template>
