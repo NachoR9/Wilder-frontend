ebff76a56ba3f6acdff250602217d4925894e5b8
NachoR9 <nachorm9@hotmail.com>
2024-09-25 19:53:37 -0700

    display videogame image

--- src/components/VideogamesList.vue
+++ src/components/VideogamesList.vue
@@ -17,7 +17,7 @@ onMounted(() => {
             <li v-for="videogame in videogameStore.videogames" class="py-8">
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="" :alt="videogame.name">
+                        <img class="w-8 h-8 rounded-full" :src="videogame.image" :alt="videogame.name">
                     </div>
                     <div class="flex-1 min-w-0 ms-12">
                         <p class="text-lg font-medium text-gray-900 truncate">
